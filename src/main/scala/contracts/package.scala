5b7d431494869b56415f8899a2bb5250b1879e42
Kirat-S <kiratgsingh@gmail.com>
2021-12-06 23:08:08 -0500

    Fixed small syntax errors in package

--- src/main/scala/contracts/package.scala
+++ src/main/scala/contracts/package.scala
@@ -21,43 +21,4 @@ package object contracts {
   def generateContractAddress(contract: ErgoContract, networkType: NetworkType): Address = {
     Address.fromErgoTree(contract.getErgoTree, networkType)
   }
-  /**
-   * Represents custom ErgoTypes to be used in SmartPooling contracts
-   */
-  object SpType {
-    final val PROPBYTES_TYPE = ErgoType.collType(ErgoType.byteType())
-    final val STRING_TYPE = ErgoType.collType(ErgoType.byteType())
-
-    final val CONSENSUS_VAL_TYPE = ErgoType.pairType[Coll[Byte], Long](PROPBYTES_TYPE, ErgoType.longType())
-    final val CONSENSUS_TYPE = ErgoType.collType(CONSENSUS_VAL_TYPE)
-
-    final val MEMBER_TYPE = ErgoType.pairType[Coll[Byte], Coll[Byte]](PROPBYTES_TYPE, STRING_TYPE)
-    final val MEMBER_LIST_TYPE = ErgoType.collType(MEMBER_TYPE)
-
-    final val POOL_FEE_TYPE = ErgoType.pairType[Coll[Byte], Int](PROPBYTES_TYPE, ErgoType.integerType())
-    final val POOL_FEES_LIST_TYPE = ErgoType.collType(POOL_FEE_TYPE)
-
-    final val POOL_INFO_TYPE = ErgoType.collType(ErgoType.longType())
-
-    final val POOL_OPERATORS_TYPE = ErgoType.collType(MEMBER_TYPE)
-
-  }
-  // SpType has three values, a normal value (used in offchain code), a conversion value(Used to convert from more traditional types)
-  // and the ergo type value is typically used to reference it.
-  // For types with parameters(like coll) we use the type parameter as the ergo type since this is how it is referenced in most methods
-  case class SpValue[A](nValue: A, cValue: _, eValue: ErgoType[_])
-
-  final val PROPBYTES_TYPE = new SpType[Coll[Byte]]()
-
-  final val PROPBYTES
-  trait SpContract {
-    val contractScript: String
-    def buildContract: ErgoContract
-
-  }
-
-
-
-
-
 }
